7c6111feec8a2c79a048d6e4ef96919d0348d53a
alsun <aminabdo8@gmail.com>
2017-05-24 01:04:43 +0200

    we finish it

---
+++ js/plugins.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Dreamer on 5/20/2017.
+ */
+$(document).ready(function () {
+    $(function(){
+        $(".bluecircle").percircle(
+            {
+                progressBarColor : "#82cefa",
+                secs:500,
+                percent :70
+            }
+        );
+    });
+});
